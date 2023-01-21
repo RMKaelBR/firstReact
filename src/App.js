d3c0eb627076dee3172c46b7fd352b4880a2afda
RMKaelBR <116938050+RMKaelBR@users.noreply.github.com>
2023-01-21 12:29:49 +0800

    added text in the p element
    aaargh!

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. PLEASE!!!!!!!
         </p>
         <h1>GITHUB IS JUST UUUGGGHHHH. LOVE HATE RELATIONSHIP</h1>
         <a
